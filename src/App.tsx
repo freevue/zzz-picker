8155141b6b8a632bea46cdcafd4e17553fe0a9ce
freevuehub <hackish113@gamil.com>
2025-10-08 03:09:46 +0900

    feat: App 컴포넌트에 규칙 섹션을 추가하여 사용자에게 정보를 제공함.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,14 @@ import { Agent, RineUp, Ban } from './components'
 function App() {
   return (
     <>
+      <div className="dark:text-white fixed top-0 left-0 w-[300px] h-full">
+        <h2>Rule</h2>
+        <div>
+          <p>lorem ipsum dolor sit amet</p>
+          <p>lorem ipsum dolor sit amet</p>
+          <p>오늘은 여기까지</p>
+        </div>
+      </div>
       <div className="p-4 w-fit ml-auto mr-[300px]">
         <div className="flex items-center gap-10">
           <RineUp />
