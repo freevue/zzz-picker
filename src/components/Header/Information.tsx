926502a83360e3bec42fd84ac4a4e16d30d292e0
freevuehub <hackish113@gamil.com>
2025-10-08 21:45:33 +0900

    feat: 정보 다이얼로그 내용 업데이트 및 링크 추가, 스타일 개선

--- src/components/Header/Information.tsx
+++ src/components/Header/Information.tsx
@@ -7,17 +7,46 @@ type Props = {
 const Information: React.FC<Props> = (props) => {
   return (
     <UI.Dialog onClose={props.onClose}>
-      <div className="bg-base dark:text-white w-96 p-4">
-        <h2 className="text-3xl font-black">Information</h2>
-        <p className="text-sm">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
-        </p>
-        <p className="text-sm">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
-        </p>
-        <p className="text-sm">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
-        </p>
+      <div className="bg-base dark:text-white w-xl p-4">
+        <h2 className="text-3xl font-black italic mb-10">Information</h2>
+        <ul className="flex flex-col gap-2">
+          <li className="flex gap-2 text-lg">
+            <span className="font-bold">호요랩:</span>
+            <a className="hover:underline" href="https://www.hoyolab.com/home" target="_blank">
+              https://www.hoyolab.com/home
+            </a>
+          </li>
+          <li className="flex gap-2 text-lg">
+            <span className="font-bold">시청자 참여:</span>
+            <a
+              className="hover:underline"
+              href="https://playsquad.gg/p/nzoetv/home"
+              target="_blank"
+            >
+              https://playsquad.gg/p/nzoetv/home
+            </a>
+          </li>
+          <li className="flex gap-2 text-lg">
+            <span className="font-bold">쉘터:</span>
+            <a
+              className="hover:underline"
+              href="https://shelter.id/MChIkVLcTWWBohXm0"
+              target="_blank"
+            >
+              https://shelter.id/MChIkVLcTWWBohXm0
+            </a>
+          </li>
+          <li className="flex gap-2 text-lg">
+            <span className="font-bold">버그 제보:</span>
+            <a
+              className="hover:underline"
+              href="https://github.com/freevue/zzz-picker/issues"
+              target="_blank"
+            >
+              https://github.com/freevue/zzz-picker/issues
+            </a>
+          </li>
+        </ul>
       </div>
     </UI.Dialog>
   )
