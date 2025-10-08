eaa3ec1eb8e3ba6ef48f3e45fc8541ea03a3631d
freevuehub <hackish113@gamil.com>
2025-10-08 21:39:28 +0900

    feat: 공용 무대 입력 기능 추가 및 경기 규칙 섹션 개선, 테이블 형식으로 캐릭터 및 엔진 비용 정보 표시

--- src/components/RuleBook/index.tsx
+++ src/components/RuleBook/index.tsx
@@ -1,14 +1,126 @@
 import Ban from './Ban'
+import { UI } from '@/components'
+import { pipe } from '@fxts/core'
+import { useEffect, useState } from 'react'
 
 const RuleBook = () => {
+  const [commonStage, setCommonStage] = useState('')
+
+  const onStageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    pipe(
+      window.location.search,
+      (query) => new URLSearchParams(query),
+      (params) => {
+        setCommonStage(event.target.value)
+        params.set('commonStage', event.target.value)
+        window.history.pushState({}, '', `?${params.toString()}`)
+      }
+    )
+  }
+
+  useEffect(() => {
+    pipe(
+      window.location.search,
+      (query) => new URLSearchParams(query),
+      (params) => params.get('commonStage'),
+      (commonStage) => {
+        setCommonStage(commonStage || '')
+      }
+    )
+  }, [])
+
   return (
-    <div className="dark:text-white p-4 bg-base w-md">
+    <div className="dark:text-white p-4 bg-base w-md flex flex-col overflow-y-auto overflow-x-hidden scrollbar-hidden">
+      <div className="mb-10">
+        <h2 className="text-3xl text-primary font-black mb-4">공용 무대</h2>
+        <UI.Input defaultValue={commonStage} onChange={onStageChange} />
+      </div>
       <Ban />
-      <div className="mt-10">
+      <div className="mt-10 flex-1">
         <h2 className="text-3xl text-primary font-black mb-4">경기 규칙</h2>
         <div>
-          <p>lorem ipsum dolor sit amet</p>
-          <p>lorem ipsum dolor sit amet</p>
+          <h3 className="text-xl text-white font-bold mb-2">Cost 설정</h3>
+          <div className="mb-4">
+            <h4>캐릭터</h4>
+            <table className="w-full overflow-hidden">
+              <colgroup>
+                <col width="33%" />
+                <col width="33%" />
+              </colgroup>
+              <thead>
+                <tr>
+                  <th className="border-1 border-gray-700 bg-gray-700 p-2">등급</th>
+                  <th className="border-1 border-gray-700 bg-gray-700 p-2">명함</th>
+                  <th className="border-1 border-gray-700 bg-gray-700 p-2">돌파당</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th className="border-1 border-gray-700 bg-gray-700">S급 픽업</th>
+                  <td className="text-center border-r-1 border-b-1 border-gray-700 font-bold p-2">
+                    +1
+                  </td>
+                  <td className="text-center border-r-1 border-b-1 border-gray-700 font-bold p-2">
+                    +1
+                  </td>
+                </tr>
+                <tr>
+                  <th className="border-1 border-gray-700 bg-gray-700">S급 상시</th>
+                  <td className="text-center border-r-1 border-b-1 border-gray-700 font-bold p-2">
+                    +1
+                  </td>
+                  <td className="text-center border-r-1 border-b-1 border-gray-700 font-bold p-2">
+                    0
+                  </td>
+                </tr>
+                <tr>
+                  <th className="border-1 border-gray-700 bg-gray-700">A급 상시</th>
+                  <td className="text-center border-r-1 border-b-1 border-gray-700 font-bold p-2">
+                    0
+                  </td>
+                  <td className="text-center border-r-1 border-b-1 border-gray-700 font-bold p-2">
+                    0
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="mb-4">
+            <h4>엔진</h4>
+            <table className="w-full overflow-hidden">
+              <colgroup>
+                <col width="33%" />
+                <col width="33%" />
+              </colgroup>
+              <thead>
+                <tr>
+                  <th className="border-1 border-gray-700 bg-gray-700 p-2">등급</th>
+                  <th className="border-1 border-gray-700 bg-gray-700 p-2">명함</th>
+                  <th className="border-1 border-gray-700 bg-gray-700 p-2">돌파당</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th className="border-1 border-gray-700 bg-gray-700 p-2">S급</th>
+                  <td className="text-center border-r-1 border-b-1 border-gray-700 font-bold p-2">
+                    +1
+                  </td>
+                  <td className="text-center border-r-1 border-b-1 border-gray-700 font-bold p-2">
+                    0
+                  </td>
+                </tr>
+                <tr>
+                  <th className="border-1 border-gray-700 bg-gray-700 p-2">A급</th>
+                  <td className="text-center border-r-1 border-b-1 border-gray-700 font-bold p-2">
+                    0
+                  </td>
+                  <td className="text-center border-r-1 border-b-1 border-gray-700 font-bold p-2">
+                    0
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
