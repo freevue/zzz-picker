6b1f577212839a8b1af739a0dd91065d3ec4c0f5
freevue <hackish113@gmail.com>
2025-11-04 20:06:50 +0900

    feat: Form.Input 스토리에 name prop 추가 및 Nickname 컴포넌트 렌더링 추가

--- apps/storybook/src/stories/Form.Input.stories.tsx
+++ apps/storybook/src/stories/Form.Input.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
-import { Form } from '@zzz-picker/components/v2'
+import { Form, Typo } from '@zzz-picker/components/v2'
 import { useState } from 'react'
 
 const meta = {
@@ -14,6 +14,7 @@ const meta = {
   },
   args: {
     value: '',
+    name: 'input',
   },
 } satisfies Meta<typeof Form.Input>
 
@@ -21,8 +22,31 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Input: Story = {
+export const Default: Story = {
   args: {
     value: 'Hello, world!',
+  },
+  render: (args) => {
+    const [value, setValue] = useState(args.value)
+
+    return <Form.Input {...args} value={value} onChange={setValue} />
+  },
+}
+
+export const Name: Story = {
+  args: {
+    value: '',
+    placeholder: '닉네임을 입력해주세요',
+  },
+  render: (args) => {
+    const [value, setValue] = useState(args.value)
+
+    return (
+      <div className="flex gap-4 items-center">
+        <Form.Nickname {...args} side="A" value={value} onChange={setValue} />
+        <Typo.Heading className="heading-3xl">VS</Typo.Heading>
+        <Form.Nickname {...args} side="B" value={value} onChange={setValue} />
+      </div>
+    )
   },
 }
