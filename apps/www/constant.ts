a68cc7ca8a1fa7f8f600ce405d4d8fd64698303c
freevue <hackish113@gmail.com>
2025-11-05 00:41:02 +0900

    feat: 새로운 링크 상수 추가

---
+++ apps/www/constant.ts
@@ -0,0 +1,17 @@
+export const LINKS = [
+  {
+    href: `/original`,
+    url: '/images/main/1.png',
+    title: '정식 로프꾼',
+  },
+  {
+    href: `/legend`,
+    url: '/images/main/3.png',
+    title: '레전드 로프꾼',
+  },
+  {
+    href: `/unlimited`,
+    url: '/images/main/4.png',
+    title: '공허사냥꾼',
+  },
+]
