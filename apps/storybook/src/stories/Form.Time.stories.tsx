9360599e3437af3538fe17d0820e0da8416e5777
freevue <hackish113@gmail.com>
2025-11-04 21:34:42 +0900

    feat: Form.Time 스토리에 상태 관리 추가 및 렌더링 최적화

--- apps/storybook/src/stories/Form.Time.stories.tsx
+++ apps/storybook/src/stories/Form.Time.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
 import { Form } from '@zzz-picker/components/v2'
+import { useEffect, useState } from 'react'
 
 const meta = {
   title: 'Form/Time',
@@ -23,5 +24,14 @@ type Story = StoryObj<typeof meta>
 export const Time: Story = {
   args: {
     value: 0,
+  },
+  render: (args) => {
+    const [value, setValue] = useState(args.value)
+
+    useEffect(() => {
+      setValue(args.value)
+    }, [args.value])
+
+    return <Form.Time {...args} value={value} onChange={setValue} />
   },
 }
