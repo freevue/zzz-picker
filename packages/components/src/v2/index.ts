4e630d20745e768857315397008bc654f1e1f625
freevue <hackish113@gmail.com>
2025-11-05 12:30:41 +0900

    feat: useRoundedSize 함수에 reverse prop 추가 및 크기별 라운딩 클래스 로직 개선

--- packages/components/src/v2/index.ts
+++ packages/components/src/v2/index.ts
@@ -7,21 +7,38 @@ export { default as Dialog } from './Dialog'
 export { default as Tabs } from './Tabs'
 export { default as Table } from './Table'
 
-export const useRoundedSize = (size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl') => {
+export const useRoundedSize = (size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl', reverse?: boolean) => {
+  const rounded = useMemo(() => {
+    switch (size) {
+      case 'xs':
+        return reverse ? 'rounded-tl-2xl rounded-br-2xl' : 'rounded-bl-2xl rounded-tr-2xl'
+      case 'sm':
+        return reverse ? 'rounded-tl-3xl rounded-br-3xl' : 'rounded-bl-3xl rounded-tr-3xl'
+      case 'md':
+        return reverse ? 'rounded-tl-3xl rounded-br-3xl' : 'rounded-bl-3xl rounded-tr-3xl'
+      case 'lg':
+        return reverse ? 'rounded-tl-4xl rounded-br-4xl' : 'rounded-bl-4xl rounded-tr-4xl'
+      case 'xl':
+        return reverse ? 'rounded-tl-4xl rounded-br-4xl' : 'rounded-bl-4xl rounded-tr-4xl'
+      default:
+        return reverse ? 'rounded-tl-2xl rounded-br-2xl' : 'rounded-bl-3xl rounded-tr-3xl'
+    }
+  }, [size, reverse])
+
   return useMemo(() => {
     switch (size) {
       case 'xs':
-        return 'size-20 rounded-bl-2xl rounded-tr-2xl'
+        return `size-20 ${rounded}`
       case 'sm':
-        return 'size-24 rounded-bl-3xl rounded-tr-3xl'
+        return `size-24 ${rounded}`
       case 'md':
-        return 'size-28 rounded-bl-3xl rounded-tr-3xl'
+        return `size-28 ${rounded}`
       case 'lg':
-        return 'size-32 rounded-bl-4xl rounded-tr-4xl'
+        return `size-32 ${rounded}`
       case 'xl':
-        return 'size-36 rounded-bl-4xl rounded-tr-4xl'
+        return `size-36 ${rounded}`
       default:
-        return 'size-28 rounded-bl-3xl rounded-tr-3xl'
+        return `size-28 ${rounded}`
     }
-  }, [size])
+  }, [size, rounded])
 }
