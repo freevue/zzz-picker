f0684a685425ea2b0d17e69f3178bd73a9670d61
freevue <hackish113@gmail.com>
2025-11-05 11:37:36 +0900

    feat: Input 컴포넌트에 새로운 props 추가 및 onChange 핸들러 수정

--- packages/components/src/v2/Form/Input.tsx
+++ packages/components/src/v2/Form/Input.tsx
@@ -1,32 +1,42 @@
 import { concat, join, pipe } from '@fxts/core'
 
 type Props = {
-  value?: string
+  value?: string | number
   placeholder?: string
-  onChange?: (value: string) => void
+  onChange?: (value: React.ChangeEvent<HTMLInputElement>) => void
+  onFocus?: (value: React.FocusEvent<HTMLInputElement>) => void
+  onBlur?: (value: React.FocusEvent<HTMLInputElement>) => void
+  onWheel?: (value: React.WheelEvent<HTMLInputElement>) => void
+  onKeyDown?: (value: React.KeyboardEvent<HTMLInputElement>) => void
   className?: string
   name: string
   type: React.HTMLInputTypeAttribute
+  max?: number
+  min?: number
+  step?: number
 }
 
 const Input: React.FC<Props> = (props) => {
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    props.onChange?.(event.target.value)
-  }
-
   return (
     <div className={pipe(['overflow-hidden'], concat([props.className || '']), join(' '))}>
       <label className="block h-full">
         <input
           className={pipe(
-            ['bg-content', 'text-ink', 'py-2', 'px-4', 'h-full'],
+            ['bg-content', 'text-ink', 'py-2', 'px-4', 'size-full'],
             concat(['focus:outline-none']),
             join(' ')
           )}
+          max={props.max}
+          min={props.min}
+          step={props.step}
           name={props.name}
           type={props.type}
           value={props.value}
-          onChange={onChange}
+          onChange={props.onChange}
+          onFocus={props.onFocus}
+          onBlur={props.onBlur}
+          onWheel={props.onWheel}
+          onKeyDown={props.onKeyDown}
           placeholder={props.placeholder}
         />
       </label>
