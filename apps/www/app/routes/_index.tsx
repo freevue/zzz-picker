5c995881e936422a62e1f2050767ebe2982d7289
freevue <hackish113@gmail.com>
2025-11-07 22:22:55 +0900

    feat: 로컬 저장 데이터 초기화 기능 추가 및 툴팁 추가

--- apps/www/app/routes/_index.tsx
+++ apps/www/app/routes/_index.tsx
@@ -1,11 +1,17 @@
 import { pipe, zipWithIndex, map, toArray } from '@fxts/core'
 import { Link } from '@remix-run/react'
-import { Typo } from '@zzz-picker/components/v2'
+import { Typo, Tooltip } from '@zzz-picker/components/v2'
+import { STORAGE_KEY } from '@zzz-picker/constant'
 import { motion } from 'motion/react'
 import { Rule, DevLog } from '~/components'
 import { LINKS } from '~/constant'
 
 const Main: React.FC = () => {
+  const onResetClick = () => {
+    window.localStorage.removeItem(STORAGE_KEY)
+    window.location.reload()
+  }
+
   return (
     <div className="size-full flex flex-col items-center justify-center gap-10">
       <img src="/images/main/logo.png" alt="logo" className="w-32 block hover:animate-turbo" />
@@ -35,19 +41,32 @@ const Main: React.FC = () => {
           toArray
         )}
       </div>
-      <div className="flex gap-6">
-        <Rule />
-        <DevLog />
-        <button
-          className="text-ink/70 body-lg hover:text-primary cursor-pointer focus:outline-none"
-          type="button"
-          onClick={() => {
-            window.localStorage.removeItem('zzz-picker-play')
-          }}
-        >
-          로컬 스토리지 초기화
-        </button>
-      </div>
+      <ul className="flex gap-6 fixed bottom-0 left-1/2 -translate-x-1/2 py-5">
+        <li className="border-r border-ink/50 pr-6">
+          <Rule />
+        </li>
+        <li className="border-r border-ink/50 pr-6">
+          <DevLog />
+        </li>
+        <li>
+          <Tooltip
+            placement="top"
+            tip={
+              <Typo.Body className="body-sm">
+                로컬에 저장된 데이터를 삭제하고 초기 상태로 되돌립니다.
+              </Typo.Body>
+            }
+          >
+            <button
+              className="text-ink/70 body-lg hover:text-primary cursor-pointer focus:outline-none"
+              type="button"
+              onClick={onResetClick}
+            >
+              로컬 저장 데이터 초기화
+            </button>
+          </Tooltip>
+        </li>
+      </ul>
     </div>
   )
 }
