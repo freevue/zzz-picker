5dec915e5239dfb1bf105d5fddf03a6cf05ad7d3
freevue <hackish113@gmail.com>
2025-11-15 17:47:03 +0900

    feat: 임시 저장기능 추가

--- apps/www/components/Playground/Floating/Save.tsx
+++ apps/www/components/Playground/Floating/Save.tsx
@@ -1,13 +1,35 @@
 import { pipe, join } from '@fxts/core'
 import { Icons } from '@zzz-picker/components'
-
-// import { usePlay } from '@zzz-picker/provider/hooks'
+import { usePlay } from '@zzz-picker/provider/hooks'
+import dayjs from 'dayjs'
 
 const Save: React.FC = () => {
-  // const { state } = usePlay()
+  const { state, cost } = usePlay()
 
   const onSaveClick = () => {
-    alert('서비스 준비중입니다.')
+    pipe(
+      {
+        state,
+        cost: { A: [...cost.A.entries()], B: [...cost.B.entries()] },
+      },
+      (data) => JSON.stringify(data, null, 2),
+      (data) => new Blob([data], { type: 'application/json' }),
+      URL.createObjectURL,
+      (url) => {
+        const link = document.createElement('a')
+
+        link.href = url
+        link.download = `zzz-picker-${dayjs().format('YYYY-MM-DD-HH-mm-ss')}.json`
+
+        return { link, url }
+      },
+      ({ link, url }) => {
+        document.body.appendChild(link)
+        link.click()
+        document.body.removeChild(link)
+        URL.revokeObjectURL(url)
+      }
+    )
   }
 
   return (
