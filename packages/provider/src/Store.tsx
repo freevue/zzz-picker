9959053d0ccbaf7a7bae54db801bb07ec6260e0e
freevue <hackish113@gmail.com>
2025-11-15 15:16:31 +0900

    feat: 엔진 데이터 쿼리 추가 및 상태 관리 개선

--- packages/provider/src/Store.tsx
+++ packages/provider/src/Store.tsx
@@ -4,8 +4,11 @@ import {
   AGENT_LIST,
   BOSS_LIST,
   DEADLY_ASSAULT_LIST,
+  ENGINE_LIST,
   useQuery,
   type GQL_AgentList,
+  type GQL_EngineList,
+  type GQL_Engine,
   type GQL_Agent,
   type GQL_BossList,
   type GQL_Boss,
@@ -21,6 +24,7 @@ type Props = {
 type State = {
   agents: Map<number, GQL_Agent>
   gqlBosses: Map<number, GQL_Boss<Array<GQL_Attribute>>>
+  gqlEngines: Map<number, GQL_Engine>
   loading: boolean
   deadlyAssaultList: Array<DeadlyAssault & { open: Dayjs }>
 }
@@ -28,11 +32,13 @@ type State = {
 export const Context = createContext<State>({
   agents: new Map(),
   gqlBosses: new Map(),
+  gqlEngines: new Map(),
   loading: false,
   deadlyAssaultList: [],
 })
 
 const Provider = (props: Props) => {
+  const { loading: engineLoading, data: engineData } = useQuery<GQL_EngineList>(ENGINE_LIST)
   const { loading: agentLoading, data: agentData } = useQuery<GQL_AgentList>(AGENT_LIST)
   const { loading: bossLoading, data: bossData } = useQuery<GQL_BossList>(BOSS_LIST)
   const { loading: deadlyAssaultLoading, data: deadlyAssaultData } =
@@ -42,9 +48,24 @@ const Provider = (props: Props) => {
     <Context.Provider
       value={{
         loading: useMemo(
-          () => agentLoading || bossLoading || deadlyAssaultLoading,
-          [agentLoading, bossLoading, deadlyAssaultLoading]
+          () => agentLoading || bossLoading || deadlyAssaultLoading || engineLoading,
+          [agentLoading, bossLoading, deadlyAssaultLoading, engineLoading]
         ),
+        gqlEngines: useMemo(() => {
+          const currentMap = new Map()
+
+          if (engineLoading) return currentMap
+
+          pipe(
+            engineData?.enginesCollection?.edges || [],
+            map(prop('node')),
+            each((engine) => {
+              currentMap.set(Number(engine.id), engine)
+            })
+          )
+
+          return currentMap
+        }, [engineData, engineLoading]),
         agents: useMemo(() => {
           const currentMap = new Map()
 
