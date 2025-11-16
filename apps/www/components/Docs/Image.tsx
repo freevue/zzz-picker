3d8dcead3eabf8108e1a70ff692243fa1cfc1296
freevue <hackish113@gmail.com>
2025-11-16 15:48:59 +0900

    feat: 컴포넌트 추가

---
+++ apps/www/components/Docs/Image.tsx
@@ -0,0 +1,23 @@
+import { pipe, join, concat } from '@fxts/core'
+
+type Props = {
+  src: string
+  alt: string
+  className?: string
+}
+
+const Image: React.FC<Props> = (props) => {
+  return (
+    <div
+      className={pipe(
+        ['w-full', 'overflow-hidden', 'rounded-bl-2xl', 'rounded-tr-2xl'],
+        concat([props.className || '']),
+        join(' ')
+      )}
+    >
+      <img src={props.src} alt={props.alt} className="block w-full" />
+    </div>
+  )
+}
+
+export default Image
