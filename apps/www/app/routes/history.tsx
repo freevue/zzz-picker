71905f8c42223e1e4920ea94c1867055f679443f
freevue <hackish113@gmail.com>
2025-11-30 20:15:19 +0900

    feat: 히스토리 컴포넌트에 authKey 리스트 표시 기능 추가

--- apps/www/app/routes/history.tsx
+++ apps/www/app/routes/history.tsx
@@ -1,18 +1,41 @@
+import { map, peek, pipe, toArray } from '@fxts/core'
 import { useStore } from '@zzz-picker/provider/hooks'
-import { useEffect } from 'react'
+import dayjs from 'dayjs'
+import { useEffect, useState } from 'react'
 
 const History: React.FC = () => {
-  const { getHistory } = useStore()
+  const { getHistory, getAuthKey } = useStore()
+  const [authKeyList, setAuthKeyList] = useState<
+    Array<{ id: string; nameKo: string; version: string; createdAt: string }>
+  >([])
 
   useEffect(() => {
-    getHistory('4a197b31-abd6-4e5b-a8ba-7818c96b51cb').then((history) => {
-      console.log(history)
-    })
+    // getHistory('4a197b31-abd6-4e5b-a8ba-7818c96b51cb').then((history) => {
+    //   console.log(history)
+    // })
+
+    pipe(
+      getAuthKey(),
+      map((auth) => ({ ...auth, createdAt: dayjs(auth.createdAt).format('YYYY-MM-DD') })),
+      toArray,
+      (authKeyList) => setAuthKeyList(authKeyList)
+    )
   }, [])
 
   return (
     <div className="size-full flex flex-col items-center justify-center gap-10">
-      <div className="w-full h-full overflow-auto scrollbar-hidden">History</div>
+      {/* <div className="w-full h-full overflow-auto scrollbar-hidden">History</div> */}
+      <ul>
+        {pipe(
+          authKeyList,
+          map((auth) => (
+            <li key={auth.id} className="body-lg text-ink">
+              <span>{auth.nameKo}</span>/<span>{auth.version}</span>/<span>{auth.createdAt}</span>
+            </li>
+          )),
+          toArray
+        )}
+      </ul>
     </div>
   )
 }
