118aecb120851b8396e74b28052a22ac31ee1641
freevue <hackish113@gmail.com>
2025-12-19 18:52:56 +0900

    feat: history v2 라우트 로직을 업데이트했습니다.

--- apps/www/app/routes/history.v2.tsx
+++ apps/www/app/routes/history.v2.tsx
@@ -10,61 +10,70 @@ const HistoryPage: React.FC = () => {
       <History.AppleTitle title="엔강대 시즌2" />
       <History.AppleSection>
         <Typo.Body className="text-ink body-3xl text-center">
-          시즌1의 통계와는 다른 재미있는 부분을 가지고 왔습니다.
+          시즌1은 순수 통계 데이터만 소개해 드렸습니다.
         </Typo.Body>
         <Typo.Body className="text-ink body-3xl text-center">
-          시즌2에는 어떤 일이 있었는지 확인해보겠습니다.
+          시즌2는 조금 다른 통계를 가지고 왔습니다.
         </Typo.Body>
         <Typo.Body className="text-ink body-3xl text-center">
-          (개발자 취향 100% 존중 데이터)
+          (그저 100% 개발자 취향 데이터)
         </Typo.Body>
       </History.AppleSection>
-      <History.AppleTitle title="좀.. 너무하던데요?" />
+      <History.AppleTitle title="너무하다.. 너무해.." />
       <History.AppleSection>
         <Typo.Body className="text-ink body-3xl text-center">
-          주로 강습전에 사용되는 캐릭터는 정해져있습니다.
+          강습전에 인기 있는 캐릭터는 정해져있습니다.
         </Typo.Body>
         <Typo.Body className="text-ink body-3xl text-center">
-          그러다보니 벤 또한 정해져있습니다.
+          자연스럽게 그 캐릭터들은 밴율이 올라갑니다.
         </Typo.Body>
         <div className="flex flex-col gap-3 items-center">
           <img className="w-lg block" src={agents.get(125210)!.banner.url} alt="카이사르" />
           <Typo.Body className="text-ink body-3xl text-center">
-            카이사르는 총 3번 버림받으면서 제일 많이 벤되었습니다.
+            카이사르는 총 3회 밴으로 가장 높은 밴율을 기록했습니다.
+            <br />
+            (심지어 단독임..)
           </Typo.Body>
         </div>
       </History.AppleSection>
-      <History.AppleTitle title="드디어 휴고의 붐은 오나?" />
+      <History.AppleTitle title="과연 휴고의 봄은 올까?" />
       <History.AppleSection>
         <Typo.Body className="text-ink body-3xl text-center">
-          이번에 휴고는 픽업기간을 가졌습니다.
+          휴고는 이번에 픽업 기간을 가지게 되었습니다.
         </Typo.Body>
         <Typo.Body className="text-ink body-3xl text-center">
-          다이아린이라는 버프도 받으며, 혹시나 하는 많은 기대감도 받았습니다.
+          다이아린과 같이 활용하는 방법으로 많은 기대감을 모았습니다.
         </Typo.Body>
         <Typo.Body className="text-ink body-3xl text-center">
-          결산이라는 매커니즘이 참 재미있는 캐릭터이지요.
+          '결산' 매커니즘으로 굉장히 재미있게 게임을 즐길 수 있는 캐릭터죠.
         </Typo.Body>
         <div className="flex flex-col gap-3 items-center">
           <img className="w-lg block" src={agents.get(154609)!.banner.url} alt="휴고" />
           <Typo.Body className="text-ink body-3xl text-center">
-            총 13번의 경기중 1번 선택되었습니다. (픽율: 7.69%)
+            13경기 중 총 1번 선택받았습니다. (픽률: 7.69%)
           </Typo.Body>
         </div>
       </History.AppleSection>
-      <History.AppleTitle title="그러면 다이아린은?" />
+      <History.AppleTitle title="그렇다면 다이아린은?" />
       <History.AppleSection>
         <Typo.Body className="text-ink body-3xl text-center">
-          휴고와 같은 픽업기간을 가진 귀염둥이 다이라린입니다.
+          이번 신규 캐릭터로 나온 다이아린입니다.
         </Typo.Body>
         <Typo.Body className="text-ink body-3xl text-center">
-          아... 그런데 다이아린... 진짜 너무 이쁘게 나왔음...
+          그런데... 다이아린 진짜 너무 이쁘게 나왔음...
         </Typo.Body>
-        <Typo.Body className="text-ink body-3xl text-center">다이아린... 진짜 이쁨...</Typo.Body>
+        <Typo.Body className="text-ink body-3xl text-center">
+          진짜... 진짜 디자인부터 성능이랑 재미까지 너무 매력적임...
+        </Typo.Body>
         <div className="flex flex-col gap-3 items-center">
-          <img className="w-lg block" src={agents.get(160142)!.banner.url} alt="다이아린" />
+          {/* <img className="w-lg block" src={agents.get(160142)!.banner.url} alt="다이아린" /> */}
+          <img
+            className="w-6xl block rounded-tr-4xl rounded-bl-4xl overflow-hidden"
+            src="/images/Dialyn/09.png"
+            alt="다이아린"
+          />
           <Typo.Body className="text-ink body-3xl text-center">
-            총 13번의 경기중 11번 선택되었습니다. (픽율: 84.62%)
+            13경기 중 총 11번 선택받았습니다. (픽률: 84.62%)
           </Typo.Body>
         </div>
       </History.AppleSection>
@@ -73,13 +82,13 @@ const HistoryPage: React.FC = () => {
         <div className="flex flex-col gap-3 items-center">
           <img className="w-lg block" src={agents.get(156728)!.banner.url} alt="앨리스" />
           <Typo.Body className="text-ink body-3xl text-center">
-            그래도 얘가 제일 이쁘다. <br />총 13번의 경기중 4번 선택되었습니다. (픽율: 30.77%)
+            앨리스는 13경기 중 총 4번 선택받았습니다.
           </Typo.Body>
         </div>
         <div className="flex flex-col gap-3 items-center">
           <img
-            className="w-lg block"
-            src="https://act-webstatic.hoyoverse.com/event-static-hoyowiki-admin/2025/08/04/c59b51460d98d087a33ef9d73b554f71_1193586350942325219.png?x-oss-process=image%2Fformat%2Cwebp"
+            className="w-6xl block rounded-tr-4xl rounded-bl-4xl overflow-hidden"
+            src="/images/04.jpg"
             alt="앨리스"
           />
           <Typo.Body className="text-ink body-3xl text-center">히히...</Typo.Body>
@@ -95,10 +104,18 @@ const HistoryPage: React.FC = () => {
         <div className="flex flex-col gap-3 items-center">
           <img
             className="w-6xl block rounded-tr-4xl rounded-bl-4xl overflow-hidden"
+            src="/images/15.jpg"
+            alt="앨리스"
+          />
+          <Typo.Body className="text-ink body-3xl text-center">끗! 모두 3영창 받으세요~</Typo.Body>
+        </div>
+        <div className="flex flex-col gap-3 items-center">
+          <img
+            className="w-6xl block rounded-tr-4xl rounded-bl-4xl overflow-hidden"
             src="/images/14.jpg"
             alt="앨리스"
           />
-          <Typo.Body className="text-ink body-3xl text-center">끗!! 모두 3영창 하시길</Typo.Body>
+          <Typo.Body className="text-ink body-3xl text-center">이쁘긴해</Typo.Body>
         </div>
       </History.AppleSection>
     </div>
