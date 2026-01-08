c1812e6a4621577e6d675de29b5a0ebe2631145e
freevue <hackish113@gmail.com>
2026-01-08 17:29:49 +0900

    [feat]: 채팅 페이지 라우트 초기 구현
    `apps/www` 프로젝트에 새로운 채팅 기능을 위한 `chat.tsx` 라우트 파일을 추가하고 기본적인 페이지 구조를 구현하였습니다.

---
+++ apps/www/app/routes/chat.tsx
@@ -0,0 +1,12 @@
+// import { supabase } from '@zzz-picker/supabase'
+import { useEffect } from 'react'
+
+// console.log(supabase)
+
+const Chat: React.FC = () => {
+  useEffect(() => {}, [])
+
+  return <div className="size-full snap-y overflow-auto scrollbar-hidden">Chat</div>
+}
+
+export default Chat
