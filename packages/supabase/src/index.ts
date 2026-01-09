b15e8afd052a8e8c835ff33ba3bfc9a9c04532dc
freevue <hackish113@gmail.com>
2026-01-09 12:09:06 +0900

    .

--- packages/supabase/src/index.ts
+++ packages/supabase/src/index.ts
@@ -1,3 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
+console.log(process.env.SUPABASE_URL)
+
 export const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!)
