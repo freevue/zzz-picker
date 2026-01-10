d5c62b84af474a1dad51821bb1661899cba91686
freevue <hackish113@gmail.com>
2026-01-10 20:17:03 +0900

    Update _index.tsx

--- apps/www/app/routes/_index.tsx
+++ apps/www/app/routes/_index.tsx
@@ -14,7 +14,11 @@ const Main: React.FC = () => {
   }
 
   return (
-    <div className="size-full flex flex-col items-center justify-center gap-10">
+    <div className="size-full flex flex-col items-center justify-center gap-10" style={{
+      backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.5)), url(https://fastcdn.hoyoverse.com/content-v2/plat/161616/3eac4aa…_5273261316780784121.jpeg)",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+    }}>
       <img src="/images/main/logo.png" alt="logo" className="w-32 block hover:animate-turbo" />
       <div className="flex items-center justify-center gap-10">
         {pipe(
