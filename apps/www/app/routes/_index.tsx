a09f8d75bde8626f09e20a9d3396ad5b43aae4d0
freevue <hackish113@gmail.com>
2026-01-21 17:32:40 +0900

    refactor: 배경 이미지 스타일링을 인라인 스타일에서 Tailwind CSS 클래스로 변경했습니다.

--- apps/www/app/routes/_index.tsx
+++ apps/www/app/routes/_index.tsx
@@ -15,12 +15,10 @@ const Main: React.FC = () => {
 
   return (
     <div
-      className="size-full flex flex-col items-center justify-center gap-10"
+      className="size-full flex flex-col items-center justify-center gap-10 bg-center bg-no-repeat bg-cover"
       style={{
         backgroundImage:
           "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)), url('https://images.zzz.freevue.dev/images/background/1d986a52-eecd-4077-8029-ce027a551cd2.jpg')",
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
       }}
     >
       <img src="/images/main/logo.png" alt="logo" className="w-10 block fixed left-10 top-10" />
