6b6f0139e9853e6d00d7aaaaab0c669d86a95b0a
freevue <hackish113@gmail.com>
2026-02-18 21:35:07 +0900

    Update _index.tsx

--- apps/www/app/routes/_index.tsx
+++ apps/www/app/routes/_index.tsx
@@ -43,7 +43,7 @@ const Main: React.FC = () => {
       }}
     >
       <img src="/images/main/logo.png" alt="logo" className="w-10 block fixed left-10 top-10" />
-      <div className="flex flex-col gap-4 fixed left-1/2 bottom-2 -translate-x-1/2">
+      <div className="flex gap-4 fixed left-1/2 bottom-2 -translate-x-1/2">
         {pipe(
           LINKS,
           zipWithIndex,
@@ -54,7 +54,7 @@ const Main: React.FC = () => {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.2, delay: index * 0.2 }}
             >
-              <Link to={link.href} className="text-center flex items-center gap-6 group">
+              <Link to={link.href} className="text-center flex flex-col items-center gap-6 group">
                 <div className="flex size-48 items-center justify-center bg-content card p-2">
                   <img
                     src={link.url}
