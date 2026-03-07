98fe6dbce8d46b1c506ade0a70495eb4a9a45308
freevue <hackish113@gmail.com>
2026-03-07 18:59:32 +0900

    Update _index.tsx

--- apps/www/app/routes/_index.tsx
+++ apps/www/app/routes/_index.tsx
@@ -39,7 +39,7 @@ const Main: React.FC = () => {
       className="size-full flex flex-col items-center justify-center gap-10 bg-center bg-no-repeat bg-cover"
       style={{
         backgroundImage:
-          "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)), url('https://images.zzz.freevue.dev/images/background/d45cc90b-5c45-48de-b104-661d09dfd90d.webp')",
+          "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)), url('https://images.zzz.freevue.dev/images/background/f9eb4038-eec0-4c8b-b147-e7eb90967510.jpg')",
       }}
     >
       <img src="/images/main/logo.png" alt="logo" className="w-10 block fixed left-10 top-10" />
