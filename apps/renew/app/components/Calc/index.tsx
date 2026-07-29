b0fb5297012426793791e72fb79f8c099af3f57c
freevue <hackish113@gmail.com>
2026-07-30 03:01:56 +0900

    [refactor]: Calc 메인 컨테이너 높이 단위 dvh로 변경
    - Calc 메인 레이아웃 높이(height) 클래스를 h-full에서 동적 뷰포트 단위인 h-dvh로 변경하였습니다.

--- apps/renew/app/components/Calc/index.tsx
+++ apps/renew/app/components/Calc/index.tsx
@@ -13,7 +13,7 @@ const Calc = () => {
   }
 
   return (
-    <div className="flex flex-col h-full w-full p-4">
+    <div className="flex flex-col h-dvh w-full p-4">
       <h1 className="ft-ria text-5xl text-primary">Cost 계산기</h1>
       <div className="flex-1 gap-20 flex h-full w-full max-w-md mx-auto flex-col justify-center">
         <AgentList key="1" onChange={onCostChange1} />
