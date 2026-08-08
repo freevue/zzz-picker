1997bf6a18eda2f42cfeb593faa25ccfecf1768d
freevue <hackish113@gmail.com>
2026-08-08 15:59:16 +0900

    [feat]: 엔강대 메타 태그 및 Google Analytics 스크립트 추가
    - apps/renew/app/root.tsx에 Title, Open Graph, Twitter Card 등 SEO 메타 태그 추가
    - Google Analytics(gtag.js) 추적 스크립트 태그 추가

--- apps/renew/app/root.tsx
+++ apps/renew/app/root.tsx
@@ -11,6 +11,32 @@ export const links: LinksFunction = () => [
 export const meta: MetaFunction = () => [
   { charSet: 'UTF-8' },
   { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+  { title: '젠레스 존 제로: 엔강대' },
+  { name: 'description', content: '젠레스 존 제로: 엔강대 경기 대시보드' },
+
+  // Facebook Meta Tags
+  { property: 'og:locale', content: 'ko_KR' },
+  { property: 'og:url', content: 'https://ncore.freevue.dev' },
+  { property: 'og:type', content: 'website' },
+  { property: 'og:title', content: '젠레스 존 제로: 엔강대' },
+  { property: 'og:description', content: '젠레스 존 제로: 엔강대 경기 대시보드' },
+  {
+    property: 'og:image',
+    content:
+      'https://images.zzz.freevue.dev/images/background/618031f1-6871-44a3-9d20-cd880e74ae88.jpg',
+  },
+
+  // Twitter Meta Tags
+  { name: 'twitter:card', content: 'summary_large_image' },
+  { property: 'twitter:domain', content: 'ncore.freevue.dev' },
+  { property: 'twitter:url', content: 'https://ncore.freevue.dev' },
+  { name: 'twitter:title', content: '젠레스 존 제로: 엔강대' },
+  { name: 'twitter:description', content: '젠레스 존 제로: 엔강대 경기 대시보드' },
+  {
+    name: 'twitter:image',
+    content:
+      'https://images.zzz.freevue.dev/images/background/618031f1-6871-44a3-9d20-cd880e74ae88.jpg',
+  },
 ]
 
 const App: React.FC = () => {
@@ -19,6 +45,19 @@ const App: React.FC = () => {
       <head>
         <Meta />
         <Links />
+
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7E861ERT9C" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag() { dataLayer.push(arguments); }
+              gtag('js', new Date());
+              gtag('config', 'G-7E861ERT9C');
+            `,
+          }}
+        />
       </head>
       <body>
         <Background />
